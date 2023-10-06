e610f8b40af96e2ceac51751b7a44631b949cb77
VincentRQ <VRQ20@fsu.edu>
2023-10-06 02:12:19 -0400

    Initial commit

---
+++ src/books.js
@@ -0,0 +1,24 @@
+import img1 from './images/soichi junji ito.jpg'
+import img2 from './images/terrymancourbook.jpg'
+import img3 from './images/junji ito franken.jpg'
+
+export const books = [
+  {
+    author: 'Junji Ito',
+    title: 'Soichi: Junji Ito Story Collection',
+    img: img1,
+    id: 1,
+  },
+  {
+    author: 'Mr. Terry Lee Mancour',
+    title: 'Spellmonger: Book 1 Of The Spellmonger Series',
+    img: img2,
+    id: 2,
+  },
+  {
+    author: 'Frankenstein: Junji Ito Story Collection',
+    title: 'Junji Ito',
+    img: img3,
+    id: 3,
+  },
+]
